4e364b8eef4ad33c7c5f784441dbf7e94f467e0e
Javeria Riaz <javeriariazmalik@gmail.com>
2018-11-22 13:04:44 +0500

    User CRUD

---
+++ ExploreTandT/Models/UserAccountViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExploreTandT.Models
+{
+    public class UserAccountViewModel
+    {
+        public List<SelectedPackagesViewModel> listofpackages = new List<SelectedPackagesViewModel>();
+
+        public List<RegisterViewModel> listofusers = new List<RegisterViewModel>();
+    }
+}
