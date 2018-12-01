ff7a2f1a1a9831285006ce4c9af0471839b3cf64
Summen Zahid <summenzahid@gmail.com>
2018-12-01 15:57:26 +0500

    New Update - Contact Mail Sending

--- ExploreTandT/Controllers/HomeController.cs
+++ ExploreTandT/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using ExploreTandT.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -25,6 +29,31 @@ namespace ExploreTandT.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> Contact(ContactViewModel collection)
+        {
+            var message = new MailMessage();
+            message.To.Add(new MailAddress("Summenzahid@gmail.com")); 
+            message.From = new MailAddress(collection.Email);  
+            message.Subject ="Explore Contact";
+            message.Body = string.Format(collection.Body, "model.FromName", "model.FromEmail", "model.Message");
+            message.IsBodyHtml = true;
+
+            using (var smtp = new SmtpClient())
+            {
+                var credential = new NetworkCredential
+                {
+                    UserName = "Summenzahid@gmail.com",  
+                    Password = "Password Goes Here"  
+                };
+                smtp.Credentials = credential;
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+                await smtp.SendMailAsync(message);
+                return Redirect("Index");
+            }
         }
     }
 }
--- ExploreTandT/DBExploreEntity.Designer.cs
+++ ExploreTandT/DBExploreEntity.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Rahama Tahir\Desktop\New folder\Explore-VS-Files\ExploreTandT\DBExploreEntity.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Hp\Desktop\new clone\Explore-VS-Files\ExploreTandT\DBExploreEntity.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
